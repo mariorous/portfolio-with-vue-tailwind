5075e0a4cdd329064471e27baaf50ceb529ee72a
〽️ario <emariors@gmail.com>
2024-12-05 12:24:04 +0100

    Comments and component useless deleted

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,6 @@ import HomePage from './components/HomePage.vue';
 <template>
   <Header />
   <HomePage />
-  <!-- <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
   <Footer />
 </template>
 
